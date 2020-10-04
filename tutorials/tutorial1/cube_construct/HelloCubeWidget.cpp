9655f2151938d90602b11b4af22f6c874ad1d576
Adam <ll16a6t@leeds.ac.uk>
2020-10-04 01:01:02 +0100

    Completed tutorial 1.

--- tutorials/tutorial1/cube_construct/HelloCubeWidget.cpp
+++ tutorials/tutorial1/cube_construct/HelloCubeWidget.cpp
@@ -15,8 +15,6 @@ void HelloCubeWidget::initializeGL()
 	{ // initializeGL()
 	// set the widget background colour
 	glClearColor(0.8, 0.3, 0.3, 0.0);
-	
-
  
 	} // initializeGL()
 
@@ -51,7 +49,7 @@ void HelloCubeWidget::cube(){
     glVertex3f( 1.0, -1.0, -1.0);
     glVertex3f( 1.0,  1.0, -1.0);
     glVertex3f( 1.0,  1.0,  1.0);
-    glEnd();
+  glEnd();
 
   glColor3f(1.0,0.0,0.0);
   glBegin(GL_POLYGON);
@@ -61,6 +59,29 @@ void HelloCubeWidget::cube(){
     glVertex3f(-1.0,  1.0, -1.0);
   glEnd();
 
+  glColor3f(1.0,1.0,1.0);
+  glBegin(GL_POLYGON);
+    glVertex3f(-1.0,  1.0,  1.0);
+    glVertex3f(-1.0, -1.0,  1.0);
+    glVertex3f(-1.0, -1.0, -1.0);
+    glVertex3f(-1.0,  1.0, -1.0);
+  glEnd();
+
+  glColor3f(0.0,0.0,0.0);
+  glBegin(GL_POLYGON);
+    glVertex3f( 1.0,  1.0, -1.0);
+    glVertex3f(-1.0,  1.0, -1.0);
+    glVertex3f(-1.0,  1.0,  1.0);
+    glVertex3f( 1.0,  1.0,  1.0);
+  glEnd();
+
+  glColor3f(1.0,0,1.0);
+  glBegin(GL_POLYGON);
+    glVertex3f( 1.0, -1.0, -1.0);
+    glVertex3f(-1.0, -1.0, -1.0);
+    glVertex3f(-1.0, -1.0,  1.0);
+    glVertex3f( 1.0, -1.0,  1.0);
+  glEnd();
 
 }
 	
@@ -72,14 +93,15 @@ void HelloCubeWidget::paintGL()
 
 	// You must set the matrix mode to model view directly before enabling the depth test
       	glMatrixMode(GL_MODELVIEW);
-        glEnable(GL_DEPTH_TEST); // comment out depth test to observe the result
+        glEnable(GL_DEPTH_TEST); // comment out depth test to observe the
+        // result
 
 
 	this->cube();
 
 	glLoadIdentity();
-       	gluLookAt(2.,0.,0., 0.0,0.0,0.0, 0.0,1.0,0.0);
-	
+       	gluLookAt(-1.,1.,-1., 0.0,0.0,0.0, 0.0,1.0,0.0);
+
 	// flush to screen
 	glFlush();	
 
